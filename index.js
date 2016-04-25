16b34ba0f8627e847b00d83fabd90e94831d89ba
Jan Sorgalla <jan.sorgalla@dotsunited.de>
2016-04-25 09:05:25 +0200

    Upgrade lodash dependencies to ^4.0.0

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var merge = require('lodash.merge');
+var mergeWith = require('lodash.mergewith');
 var isArray = require('lodash.isarray');
 
 module.exports = function(object, fragment) {
-    return merge(object, fragment, function(a, b) {
+    return mergeWith(object, fragment, function(a, b) {
         if (isArray(a)) {
             return a.concat(b);
         }
